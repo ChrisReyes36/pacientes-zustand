b3faf39a45e8eade6b4d65ee00d6ba029a62ab86
Christopher Reyes <c.munoz@dinac.gob.sv>
2026-05-15 16:48:15 -0600

    feature(crud): agregar y listar pacientes

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,30 @@
+import type { Patient } from "../types";
+import PatientDetailItem from "./PatientDetailItem";
+
+type PatientDetailsProps = {
+  patient: Patient;
+};
+
+function PatientDetails({ patient }: PatientDetailsProps) {
+  return (
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatientDetailItem label="ID:" data={patient.id} />
+      <PatientDetailItem label="Paciente:" data={patient.name} />
+      <PatientDetailItem label="Propietario:" data={patient.caretaker} />
+      <PatientDetailItem label="Email:" data={patient.email} />
+      <PatientDetailItem label="Fecha Alta:" data={patient.date.toString()} />
+      <PatientDetailItem label="Síntomas:" data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">
+          Editar
+        </button>
+        <button className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PatientDetails;
---
+++ src/store.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { v4 as uuidV4 } from "uuid";
+import type { DraftPatient, Patient } from "./types";
+
+type PatientState = {
+  patients: Patient[];
+  addPatient: (data: DraftPatient) => void;
+};
+
+const createPatient = (patient: DraftPatient): Patient => ({
+  id: uuidV4(),
+  ...patient,
+});
+
+export const usePatientStore = create<PatientState>((set) => ({
+  patients: [],
+  addPatient: (data) => {
+    const newPatient = createPatient(data);
+    set((state) => ({
+      patients: [...state.patients, newPatient],
+    }));
+  },
+}));
